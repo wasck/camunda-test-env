f040a7958a7bcc5fa405cd714a0a8fc413a60266
wasck <Waldemar.Schik@gmail.com>
2020-09-04 09:50:08 +0200

    Initialize spring boot project

---
+++ src/main/java/de/wasck/camundatestapi/CamundaTestApiApplication.java
@@ -0,0 +1,13 @@
+package de.wasck.camundatestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wasck/camundatestapi/CamundaTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wasck.camundatestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
